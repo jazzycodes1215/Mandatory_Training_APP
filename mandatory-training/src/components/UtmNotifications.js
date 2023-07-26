21fc8ce5992029c21ba185969d909b550ca28519
acaldemone <acaldemone@gmail.com>
2023-07-26 12:58:35 -0600

    added groundwork for notifications

--- mandatory-training/src/components/UtmNotifications.js
+++ mandatory-training/src/components/UtmNotifications.js
@@ -1,16 +1,26 @@
-
+import { useState } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css'
+import { useNotificationCenter } from 'react-toastify/addons/use-notification-center';
 
 export default function UtmNotifications() {
+  const [notification, setNotification] = useState([]);
+  const { notifications, clear, markAllAsRead, markAsRead  } = useNotificationCenter();
 
-  
+  const showToast = () => {
+    toast('Hello World', {
+        data: {
+            title: 'Hello World Again',
+            text: 'We are here again with another article'
+        }
+    });
+};
+
   return (
     <div>
-        <div>
-          <h2>Notifications Section</h2>
-        </div>
-        <div>
-          {/* Add the sub-menu content for the Notifications section */}
-        </div>
-      </div>
+            <p>{notifications.length}</p>
+            <button onClick={showToast}>Click me</button>
+            <ToastContainer />
+    </div>
   )
 }
