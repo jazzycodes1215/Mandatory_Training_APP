0a3485cca3c1ecbc39f2a70de0b47093edb1e597
Devinekyle <133117393+Devinekyle@users.noreply.github.com>
2023-07-31 14:03:45 -0600

    Update index.js

--- back-end/index.js
+++ back-end/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-require('dotenv').config({ path: './../.env' })
+require('dotenv').config({ path: '/etc/secrets/.env' })
 const port = process.env.BACKENDPORT
 console.log(process.env);
 const knex = require('knex')(require('./knexfile.js')[process.env.DATABASESTRING ?? 'production' : 'development']);
