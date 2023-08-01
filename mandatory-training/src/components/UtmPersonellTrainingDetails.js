223068e9ced051f07e29022e4202f8f91df3dfc8
acaldemone <acaldemone@gmail.com>
2023-08-01 15:06:27 -0600

    trying to add improved functionality to the utm management

---
+++ mandatory-training/src/components/UtmPersonellTrainingDetails.js
@@ -0,0 +1,16 @@
+import React from "react";
+import { useParams } from "react-router-dom";
+
+export default function UtmPersonellTrainingDetails({ userID }) {
+  const { trainingId } = useParams();
+  console.log("userID in UtmPersonellTrainingDetails:", userID);
+  // Fetch the detailed training information based on the trainingId
+  // You can use this ID to fetch the necessary data from your API or local state
+
+  return (
+    <div>
+      <h2>Training Details for User ID: {userID}</h2>
+      <h2>Training Details for Training ID: {trainingId}</h2>
+    </div>
+  );
+}
