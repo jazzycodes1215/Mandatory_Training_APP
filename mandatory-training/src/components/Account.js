4bebf182818011ece12c2294e3da38b703eb030f
devinekyle <devinekyle@hotmail.com>
2023-08-01 13:25:49 -0600

    Merging origin and devine. Fixed account page going undefined

--- mandatory-training/src/components/Account.js
+++ mandatory-training/src/components/Account.js
@@ -170,7 +170,6 @@ export default function Account() {
                 }
             })
             .then(data=>{
-                setToken(data.token)
                 setUpdated(!updated);
             })
             .catch(err => {
