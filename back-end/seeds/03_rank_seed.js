173d3708ee26c9aed4f7f81420f69fcae1c890e9
puddles <puddleimages@gmail.com>
2023-08-03 14:30:52 -0600

    oops

--- back-end/seeds/03_rank_seed.js
+++ back-end/seeds/03_rank_seed.js
@@ -6,24 +6,24 @@ exports.seed = async function(knex) {
   // Deletes ALL existing entries
   await knex('ranks').del()
   await knex('ranks').insert([
-    {id: 1, name: 'E-1'},
-    {id: 2, name: 'E-2'},
-    {id: 3, name: 'E-3'},
-    {id: 4, name: 'E-4'},
-    {id: 5, name: 'E-5'},
-    {id: 6, name: 'E-6'},
-    {id: 7, name: 'E-7'},
-    {id: 8, name: 'E-8'},
-    {id: 9, name: 'E-9'},
-    {id: 10, name: 'O-1'},
-    {id: 11, name: 'O-2'},
-    {id: 12, name: 'O-3'},
-    {id: 13, name: 'O-4'},
-    {id: 14, name: 'O-5'},
-    {id: 15, name: 'O-6'},
-    {id: 16, name: 'O-7'},
-    {id: 17, name: 'O-8'},
-    {id: 18, name: 'O-9'},
-    {id: 19, name: 'O-10'}
+    {name: 'E-1'},
+    {name: 'E-2'},
+    {name: 'E-3'},
+    {name: 'E-4'},
+    {name: 'E-5'},
+    {name: 'E-6'},
+    {name: 'E-7'},
+    {name: 'E-8'},
+    {name: 'E-9'},
+    {name: 'O-1'},
+    {name: 'O-2'},
+    {name: 'O-3'},
+    {name: 'O-4'},
+    {name: 'O-5'},
+    {name: 'O-6'},
+    {name: 'O-7'},
+    {name: 'O-8'},
+    {name: 'O-9'},
+    {name: 'O-10'}
   ]);
 };
